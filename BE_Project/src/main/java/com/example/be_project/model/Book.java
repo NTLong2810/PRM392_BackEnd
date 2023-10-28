bad977d47f0496b00588bb8a69e6d3cca1da6f27
94m3glan <khanhhp2332@gmail.com>
2023-10-28 22:55:54 +0700

    add annotaion for Book model

--- BE_Project/src/main/java/com/example/be_project/model/Book.java
+++ BE_Project/src/main/java/com/example/be_project/model/Book.java
@@ -1,10 +1,19 @@
 package com.example.be_project.model;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Objects;
 
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 @Table(name = "Book")
 public class Book {
     @Id
