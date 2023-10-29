11a236a4bb12794bee1ef7c7893c5518647fff18
Kasa <103961248+Kawoasa@users.noreply.github.com>
2023-10-29 16:43:14 +0700

    Add BookList and BookInfo

--- BE_Project/src/main/java/com/example/be_project/api/BookController.java
+++ BE_Project/src/main/java/com/example/be_project/api/BookController.java
@@ -1,4 +1,45 @@
 package com.example.be_project.api;
 
+import com.example.be_project.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(path = "/books")
+    public ResponseEntity<?> getAllBooks() {
+        try {
+            List<Object[]> bookList = bookService.findAllBooks();
+
+            if (bookList.isEmpty()) {
+                return new ResponseEntity<>("Danh sách sách trống", HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        } catch (Exception e) {
+            // Ghi log lỗi ở đây
+            e.printStackTrace();
+            return new ResponseEntity<>("Đã xảy ra lỗi nội bộ", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("books/{bookId}")
+    public ResponseEntity<?> getBookInfoById(@PathVariable int bookId) {
+        Object bookInfo = bookService.bookDetail(bookId);
+        if (bookInfo != null) {
+            return ResponseEntity.ok(bookInfo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- BE_Project/src/main/java/com/example/be_project/service/BookService.java
+++ BE_Project/src/main/java/com/example/be_project/service/BookService.java
@@ -1,4 +1,24 @@
 package com.example.be_project.service;
 
+import com.example.be_project.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Object[]> findAllBooks() {
+        return bookRepository.findAllBook();
+    }
+
+    public Object[] bookDetail(int bookId){
+        List<Object[]> result = bookRepository.findBookDetailById(bookId);
+        if (result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
 }
