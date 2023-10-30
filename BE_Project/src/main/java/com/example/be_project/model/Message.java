7a3fbf94c93804d444ffabfc48342aa9912c6b18
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-10-30 14:22:34 +0700

    Create ChatAPI for BE

--- BE_Project/src/main/java/com/example/be_project/model/Message.java
+++ BE_Project/src/main/java/com/example/be_project/model/Message.java
@@ -25,4 +25,44 @@ public class Message {
     private int sellerId;
 
     // Getter and setter methods
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+
+    public Date getMessageDatetime() {
+        return messageDatetime;
+    }
+
+    public void setMessageDatetime(Date messageDatetime) {
+        this.messageDatetime = messageDatetime;
+    }
+
+    public int getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(int sellerId) {
+        this.sellerId = sellerId;
+    }
 }
