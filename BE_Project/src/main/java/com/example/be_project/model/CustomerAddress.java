b6e9804ff589c281502582447561c3df61468c4e
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-10-30 09:57:12 +0700

    Pull code branch Long into main

--- BE_Project/src/main/java/com/example/be_project/model/CustomerAddress.java
+++ BE_Project/src/main/java/com/example/be_project/model/CustomerAddress.java
@@ -1,13 +1,9 @@
 package com.example.be_project.model;
 
-<<<<<<< HEAD
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-=======
-import jakarta.persistence.*;
->>>>>>> b35e407cc910bbc170830f6682501c1b401af7a5
 
 @Entity
 @Table(name="Customer_Address")
@@ -18,8 +14,5 @@ public class CustomerAddress {
 
     @Column(name = "address_id")
     private int addressId;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b35e407cc910bbc170830f6682501c1b401af7a5
+
